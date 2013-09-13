91fa8d656cde2626959575d93a297ce065a67478
Gonzalo Miranda <gonzalomir@gmail.com>
2013-09-13 06:36:30 -0400

    Botones, entradas de datos por un pin

---
+++ Ejercicio02/Ejercicio02.ino
@@ -0,0 +1,19 @@
+#define led 13
+#define boton 2
+
+boolean estado=0;
+
+void setup(){
+  pinMode(led,OUTPUT);
+  pinMode(boton,INPUT);
+}
+
+void loop(){
+  estado = digitalRead(boton);
+  if (estado) {     
+    digitalWrite(led, HIGH);  
+  } 
+  else {
+    digitalWrite(led, LOW); 
+  }
+}
