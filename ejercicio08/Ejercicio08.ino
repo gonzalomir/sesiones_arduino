d4058b9a43284d1d812ff2e9010df4af3fe7320e
gonzalomir <gonzalomir@gmail.com>
2013-09-12 23:07:43 -0400

    Conversor analogo digital (Salida Vumetro)

---
+++ ejercicio08/Ejercicio08.ino
@@ -0,0 +1,20 @@
+const byte pinLDR=0;
+
+int valLDR=0;  //int 16 bits
+byte valor=0;
+
+void setup(){
+  DDRD=255;  //B11111111
+}
+
+void loop(){
+  valLDR=analogRead(pinLDR);
+  valor=map(valLDR,0,1023,0,7);
+  for(byte i=0;i<valor;i++){
+    digitalWrite(i,HIGH);
+  }
+  delay(100);
+  PORTD=0;
+}
+
+
