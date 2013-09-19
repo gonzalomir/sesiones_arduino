3276e42299fe8835f9170ad5e0552079eca75cf3
Gonzalo Miranda <gonzalomir@gmail.com>
2013-09-19 10:53:58 -0400

    Control de un Motor Paso a Paso (Caso: paso simple, y practica con arrays)

---
+++ Ejercicio13_MotorPAP_Simple/Ejercicio13_MotorPAP_Simple.ino
@@ -0,0 +1,40 @@
+const byte pines[4]={
+  2,3,4,5};
+const byte pasos[4]={
+  1,2,4,8};
+byte indice=0;
+/*
+Primer paso  1=0001
+ Segundo paso 2=0010
+ Tercer paso  4=0100
+ Cuarto paso  8=1000
+ */
+
+void setup(){
+  for(int i=0;i<4;i++){
+    pinMode(pines[i],OUTPUT);
+  }
+}
+
+void loop(){
+  avanzar(4, 250);
+}
+
+void avanzar(int num_pasos,int delay_paso){
+  for(int i=0;i<num_pasos;i++){
+    writeMotor();
+    delay(delay_paso);
+    indice++;
+    if(indice>3){
+      indice=0;
+    }
+  }
+}
+
+void writeMotor(){
+  boolean aux=false;
+  for(int i=0;i<4;i++){
+    aux=bitRead(pasos[indice],i); //lee un bit del vector pasos
+    digitalWrite(pines[i],aux);   //escribe es bit en un pin del motor
+  }
+}
